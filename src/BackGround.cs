f93b375de36f8a6666fba968db5af9b003f43ccd
microUranium <nano6123micro@gmail.com>
2025-05-05 10:17:30 +0900

    fix tab

--- src/BackGround.cs
+++ src/BackGround.cs
@@ -5,11 +5,11 @@ using System;
 
 public partial class BackGround : ParallaxBackground
 {
-    [Export]
-    public float ScrollSpeed { get; set; } // スクロール速度
+	[Export]
+	public float ScrollSpeed { get; set; } // スクロール速度
 
-    public override void _Process(double delta)
-    {
-        ScrollOffset += new Vector2((float)(ScrollSpeed * delta * -1), 0); 
-    }
+	public override void _Process(double delta)
+	{
+		ScrollOffset += new Vector2((float)(ScrollSpeed * delta * -1), 0); 
+	}
 }
