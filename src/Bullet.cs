d33c5f635deacbe844e7f96a8a9967e2618a0153
ROBO358 <18186312+ROBO358@users.noreply.github.com>
2025-05-24 15:22:41 +0900

    Refactor Bullet and BulletTest classes for consistent formatting

--- src/Bullet.cs
+++ src/Bullet.cs
@@ -5,30 +5,30 @@ using System;
 
 public partial class Bullet : Node2D
 {
-    [Export]
-    public float Speed { get; set; }
-    [Export]
-    public Vector2 Direction
-    {
-        get => _direction;
-        set => _direction = value.Normalized();
-    }
+	[Export]
+	public float Speed { get; set; }
+	[Export]
+	public Vector2 Direction
+	{
+		get => _direction;
+		set => _direction = value.Normalized();
+	}
 
-    private Vector2 _direction = Vector2.Right;
+	private Vector2 _direction = Vector2.Right;
 
-    public override void _Ready()
-    {
-        var notifier = GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D");
-        notifier.ScreenExited += OnScreenExited;
-    }
+	public override void _Ready()
+	{
+		var notifier = GetNode<VisibleOnScreenNotifier2D>("VisibleOnScreenNotifier2D");
+		notifier.ScreenExited += OnScreenExited;
+	}
 
-    private void OnScreenExited()
-    {
-        QueueFree();
-    }
+	private void OnScreenExited()
+	{
+		QueueFree();
+	}
 
-    public override void _Process(double delta)
-    {
-        Position += _direction * Speed * (float)delta;
-    }
+	public override void _Process(double delta)
+	{
+		Position += _direction * Speed * (float)delta;
+	}
 }
--- src/BulletTest.cs
+++ src/BulletTest.cs
@@ -10,48 +10,48 @@ using static Utils;
 [TestSuite]
 public class BulletTest
 {
-    [TestCase]
-    public void Direction_ShouldBeNormalized_WhenSet()
-    {
-        // GIVEN
-        var bullet = new Bullet();
+	[TestCase]
+	public void Direction_ShouldBeNormalized_WhenSet()
+	{
+		// GIVEN
+		var bullet = new Bullet();
 
-        // WHEN
-        bullet.Direction = new Vector2(10f, 0f); // Right direction
+		// WHEN
+		bullet.Direction = new Vector2(10f, 0f); // Right direction
 
-        // THEN
-        AssertVector(bullet.Direction).IsEqual(Vector2.Right);
-    }
+		// THEN
+		AssertVector(bullet.Direction).IsEqual(Vector2.Right);
+	}
 
-    [TestCase]
-    public void Process_ShouldUpdatePosition()
-    {
-        // GIVEN
-        var bullet = new Bullet();
-        bullet.Position = Vector2.Zero;
-        bullet.Speed = 200f;
-        bullet.Direction = Vector2.Right;
+	[TestCase]
+	public void Process_ShouldUpdatePosition()
+	{
+		// GIVEN
+		var bullet = new Bullet();
+		bullet.Position = Vector2.Zero;
+		bullet.Speed = 200f;
+		bullet.Direction = Vector2.Right;
 
-        // WHEN
-        bullet._Process(1.0);
+		// WHEN
+		bullet._Process(1.0);
 
-        // THEN
-        AssertVector(bullet.Position).IsEqual(new Vector2(200f, 0f));
-    }
+		// THEN
+		AssertVector(bullet.Position).IsEqual(new Vector2(200f, 0f));
+	}
 
-    [TestCase]
-    public void Process_ShouldMoveInDirectionOfVector()
-    {
-        // GIVEN
-        var bullet = new Bullet();
-        bullet.Position = Vector2.Zero;
-        bullet.Speed = 200f;
-        bullet.Direction = new Vector2(0f, 1f); // Downward direction
+	[TestCase]
+	public void Process_ShouldMoveInDirectionOfVector()
+	{
+		// GIVEN
+		var bullet = new Bullet();
+		bullet.Position = Vector2.Zero;
+		bullet.Speed = 200f;
+		bullet.Direction = new Vector2(0f, 1f); // Downward direction
 
-        // WHEN
-        bullet._Process(1.0);
+		// WHEN
+		bullet._Process(1.0);
 
-        // THEN
-        AssertVector(bullet.Position).IsEqual(new Vector2(0f, 200f));
-    }
+		// THEN
+		AssertVector(bullet.Position).IsEqual(new Vector2(0f, 200f));
+	}
 }
