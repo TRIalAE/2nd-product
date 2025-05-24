815c81a7318d5228db7443ad2eba3bf0fb5402b3
ROBO358 <18186312+ROBO358@users.noreply.github.com>
2025-05-24 14:18:41 +0900

    Add unit tests for Bullet class functionality

---
+++ src/BulletTest.cs
@@ -0,0 +1,57 @@
+using Godot;
+using GdUnit4;
+
+namespace BulletTest;
+
+using Bullet;
+using static Assertions;
+using static Utils;
+
+[TestSuite]
+public class BulletTest
+{
+    [TestCase]
+    public void Direction_ShouldBeNormalized_WhenSet()
+    {
+        // GIVEN
+        var bullet = new Bullet();
+
+        // WHEN
+        bullet.Direction = new Vector2(10f, 0f); // Right direction
+
+        // THEN
+        AssertVector(bullet.Direction).IsEqual(Vector2.Right);
+    }
+
+    [TestCase]
+    public void Process_ShouldUpdatePosition()
+    {
+        // GIVEN
+        var bullet = new Bullet();
+        bullet.Position = Vector2.Zero;
+        bullet.Speed = 200f;
+        bullet.Direction = Vector2.Right;
+
+        // WHEN
+        bullet._Process(1.0);
+
+        // THEN
+        AssertVector(bullet.Position).IsEqual(new Vector2(200f, 0f));
+    }
+
+    [TestCase]
+    public void Process_ShouldMoveInDirectionOfVector()
+    {
+        // GIVEN
+        var bullet = new Bullet();
+        bullet.Position = Vector2.Zero;
+        bullet.Speed = 200f;
+        bullet.Direction = new Vector2(0f, 1f); // Downward direction
+
+        // WHEN
+        bullet._Process(1.0);
+
+        // THEN
+        AssertVector(bullet.Position).IsEqual(new Vector2(0f, 200f));
+    }
+}
