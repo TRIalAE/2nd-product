b00aecab32b6060ef175d8848b996acfa23f89fb
microUranium <nano6123micro@gmail.com>
2025-06-22 10:09:55 +0900

    敵出現エントリとフェーズごとの出現データを追加

---
+++ src/SpawnPerPhaseData.cs
@@ -0,0 +1,9 @@
+using Godot;
+using Godot.Collections;
+
+[GlobalClass]
+public partial class SpawnPerPhaseData : Resource
+{
+    [Export]
+    public Array<EnemySpawnEntry> SpawnTable { get; set; } // このフェーズの出現テーブル
+}
